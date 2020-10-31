514f89b3622d1d6b9862085835754f6d75c041c7
Madhur Maurya <madhurmaurya365@gmail.com>
2020-10-31 11:12:56 +0530

    Basic Echo

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
 from flask import Flask, request, redirect
-from twilio.twiml.messaging_response import MessagingResponse
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/sms", methods=['GET', 'POST'])
-def incoming_sms():
-    """Send a dynamic reply to an incoming text message"""
-    # Get the message the user sent our Twilio number
-    body = request.values.get('Body', None)
+class Echo(Resource):
+    def get(self, message):
+        return {'message':message}
 
-    # Start our TwiML response
-    resp = MessagingResponse()
-
-    # Determine the right reply for this message
-    if body == 'hello':
-        resp.message("Hi!")
-    elif body == 'bye':
-        resp.message("Goodbye")
-
-    return str(resp)
+api.add_resource(Echo, '/echo/<string:message>')
 
 if __name__ == "__main__":
     app.run(debug=True)
